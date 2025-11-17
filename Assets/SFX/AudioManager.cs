058c724d5dec7869d9ae2b3b93a621851a89023a
Taron Osifo <taron.osifo1@gmail.com>
2025-11-16 21:28:13 -0600

    AudioManager script
    Created audio manager for holding the different sounds. Should play background music as well as movement sounds (theoretically). Yet to add wav/mp3s for use to test.

---
+++ Assets/SFX/AudioManager.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class AudioManager : MonoBehaviour 
+{
+    public enum SoundType //Enum to define different sound types
+    {
+        Movement, 
+        Attack,
+        BattleStart,
+        musicMain,
+        musicBattle 
+    }
+    [System.Serializable]
+    public class Sound
+    {
+        public SoundType type;
+        public AudioClip clip;
+
+        [Range(0f, 1f)]
+        public float volume = 1f;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+
+    public static AudioManager instance;//Singleton instance
+    public Sound[] allSounds; //Array to hold all sounds
+
+    private Dictionary<SoundType, Sound> _soundDictionary = new Dictionary<SoundType, Sound>();
+    private AudioSource _musicSource;
+        //Dictionary to hold sounds for quick access
+
+        private void Awake()
+    {
+        instance = this;
+
+        foreach(var s in allSounds) //Populate dictionary with sounds
+        {
+            _soundDictionary[s.type] = s; //Add sound to dictionary
+        }
+    }
+    public SoundType selectedSound; //Sound type to play on key press for testing
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+        {
+            Play(selectedSound); //Play sound on key press
+        }
+    }
+    public void Play(SoundType type)
+    {
+        if(!_soundDictionary.TryGetValue(type, out Sound s))
+        {
+            Debug.LogWarning($"Sound type {type} not found!"); //Warns if sound type is not found
+            return; 
+        }
+
+        var soundObj = new GameObject($"Sound_{type}"); //Create new GameObject to play the sound
+        var audioSource = soundObj.AddComponent<AudioSource>(); //Add AudioSource component to the new GameObject   
+
+        audioSource.clip = s.clip; //Set clip
+        audioSource.volume = s.volume; //Set volume
+
+        audioSource.Play(); //Play the sound
+
+        Destroy(soundObj, s.clip.length); //Destroys object after clip finishes playing
+
+    }
+
+    public void ChangeMusic(SoundType type)
+    {
+        if(!_soundDictionary.TryGetValue(type, out Sound track))
+        {
+            Debug.LogWarning($"Music type {type} not found!"); //Warns if sound type is not found
+            return; 
+        }
+        if(_musicSource != null)
+        {
+            var container = new GameObject("SoundTrack");
+            _musicSource = container.AddComponent<AudioSource>();
+            _musicSource.loop = true;
+        }
+        _musicSource.clip = track.clip;
+        _musicSource.Play();
+    }
+
+}
